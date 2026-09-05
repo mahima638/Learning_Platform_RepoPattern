1cbe615016c503099ce9c3047825d5c7f5ecbd69
mahima <mahimarp2004@gmail.com>
2026-09-05 20:38:22 +0530

    added migration file

---
+++ LearningPlatformRepoPattern/Migrations/20260905145442_AddAdminModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearningPlatformRepoPattern.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

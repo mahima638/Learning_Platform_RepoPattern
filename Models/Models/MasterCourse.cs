14aa38218b1feef41fd161e6fe7ffaedf803301d
Abhishek <abhisinghp13@gmail.com>
2026-09-06 02:25:29 +0530

    Add Models

---
+++ Models/Models/MasterCourse.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnigAppMVCCore.Models
+{
+    public class MasterCourse
+    {
+        [Key]
+        public int mid { get; set; }
+
+        public string mname { get; set; }
+
+        public string mstatus { get; set; }
+    }
+}
---
+++ Models/Subscriptions.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnigAppMVCCore.Models
+{
+    public class Subscriptions
+    {
+        [Key]
+        public int sub_id { get; set; }
+
+        [StringLength(100)]
+        public string? sub_type { get; set; }
+
+        public int mid { get; set; }
+
+        public double sub_amount { get; set; }
+
+        public string? subStatus { get; set; }
+
+        public string? subThumbnail { get; set; }
+
+        public MasterCourse? MasterCourse { get; set; }
+
+        public ICollection<SubscriptionSubCourse> SubscriptionSubCourses { get; set; }
+            = new List<SubscriptionSubCourse>();
+    }
+}
